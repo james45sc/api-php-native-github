ae4ed18fd4b89d7117ad2dcece0ac0266293ca37
James Feliks Gabriel <james@example.com>
2025-10-28 21:33:36 +0800

    Initial commit of the project

--- config/database.php
+++ config/database.php
@@ -1,23 +1,17 @@
 <?php
-$host = 'localhost';
-$db_name = 'github'; 
-$user = 'root';
-$password = '';
 try {
-    $dsn = "mysql:host=$host;dbname=$db_name;charset=utf8mb4";
-    
-    $options = [
-        PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-        PDO::ATTR_EMULATE_PREPARES   => false,
-    ];
-
-    $pdo = new PDO($dsn, $user, $password, $options);
-    
-    echo "Koneksi ke database berhasil!";
-    
-} catch (\PDOException $e) {
-    echo "Koneksi database GAGAL: " . $e->getMessage();
-    exit();
+  // buat koneksi dengan database
+  $dbh = new PDO('mysql:host=localhost;dbname=github', "root", "");
+  
+  // set error mode
+  $dbh->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+  
+  // hapus koneksi
+  $dbh = null;
+}
+catch (PDOException $e) {
+  // tampilkan pesan kesalahan jika koneksi gagal
+  print "Koneksi atau query bermasalah: " . $e->getMessage() . "<br/>";
+  die();
 }
 ?>
